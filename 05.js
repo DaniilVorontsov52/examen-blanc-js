4f6b1399c0c45029adec8c35fac30b4ac8b08e46
DaniilVorontsov52 <dvorontsov@edenschool.fr>
2025-11-04 17:19:26 +0100

    js exam blanck

---
+++ 05.js
@@ -0,0 +1,8 @@
+function isSumEven(a, b) {
+	if ((a + b == 0, 1, 3, 5, 7, 9)) {
+		return false;
+	} else return true;
+}
+
+console.log(isSumEven(1, 3)); // Résultat attendu: true
+console.log(isSumEven(1, 0)); // Résultat attendu: false
---
+++ 06.js
@@ -0,0 +1,8 @@
+function isEnoughMoney(price, wallet) {
+	if (wallet - price >= 0) {
+		return true;
+	} else return false;
+}
+
+console.log(isEnoughMoney(10, 0)); // Résultat attendu: false
+console.log(isEnoughMoney(5, 25)); // Résultat attendu: true
